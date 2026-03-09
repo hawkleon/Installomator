e70bc6bd5b0229407131393ee4b67269cbadadfe
hawkleon <leon.wiesen@hawk.de>
2026-03-09 16:16:11 +0100

    Update rustdesk.sh
    Überprüfung zwischen beiden Architekturen aufgenommen

--- fragments/labels/rustdesk.sh
+++ fragments/labels/rustdesk.sh
@@ -1,8 +1,17 @@
 rustdesk)
     name="RustDesk"
     type="dmg"
-    downloadURL=$(downloadURLFromGit rustdesk rustdesk)
-    appNewVersion=$(versionFromGit rustdesk rustdesk)
-    archiveName="rustesk-$appNewVersion.dmg"
+    # Architektur-Check für den richtigen Download
+    if [[ $(arch) == "arm64" ]]; then
+        # Apple Silicon (M1/M2/M3/M4)
+        downloadURL=$(curl -sfL "https://api.github.com" | grep -i "browser_download_url" | grep "aarch64.dmg" | cut -d '"' -f 4)
+    else
+        # Intel (x86_64)
+        downloadURL=$(curl -sfL "https://api.github.com" | grep -i "browser_download_url" | grep "x86_64.dmg" | cut -d '"' -f 4)
+    fi
+    # Version ohne das "v" extrahieren
+    appNewVersion=$(curl -sfL "https://api.github.com" | grep "tag_name" | cut -d '"' -f 4 | tr -d 'v')
+    archiveName="rustdesk-$appNewVersion.dmg"
     expectedTeamID="HZF9JMC8YN"
     ;;
+

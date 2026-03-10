e584e7e05736fde172a853138eb40fd7617b1ed9
hawkleon <hawkleon@users.noreply.github.com>
2026-03-10 06:51:49 +0000

    Runner compiled Installomator.sh and Labels.txt using assemble.sh

--- Installomator.sh
+++ Installomator.sh
@@ -349,7 +349,7 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
     fi
 fi
 VERSION="10.9beta"
-VERSIONDATE="2026-03-09"
+VERSIONDATE="2026-03-10"
 
 # MARK: Functions
 
@@ -8922,6 +8922,8 @@ qgis-pr)
     type="dmg"
     downloadURL="https://download.qgis.org/downloads/macos/qgis-macos-pr.dmg"
     appNewVersion="$(curl -fs "https://www.qgis.org/da/_static/documentation_options.js" | grep -i version | cut -d "'" -f2)"
+    downloadURL="$(curl -fs "https://qgis.org/download/" | grep -o "https://.*/qgis-macos-pr.dmg" | tail -1)"
+    appNewVersion="$(curl -fs https://www.qgis.org/resources/roadmap/ | grep -oE "currently supported releases: [0-9]+\.[0-9]+(\.[0-9]+) and [0-9]+\.[0-9]+(\.[0-9]+)" | awk '{print$6}')"
     expectedTeamID="4F7N4UDA22"
     ;;
 qlab)

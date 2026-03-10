7db8dc3ff307171cd09f8115005416859d821492
hawkleon <leon.wiesen@hawk.de>
2026-03-10 08:10:43 +0100

    Update rustdesk.sh
    fix downloadurl

--- fragments/labels/rustdesk.sh
+++ fragments/labels/rustdesk.sh
@@ -1,17 +1,22 @@
 rustdesk)
     name="RustDesk"
     type="dmg"
+    # Korrekte API-URL für das RustDesk-Repository
+    apiURL="https://api.github.com"
+    
     # Architektur-Check für den richtigen Download
     if [[ $(arch) == "arm64" ]]; then
         # Apple Silicon (M1/M2/M3/M4)
-        downloadURL=$(curl -sfL "https://api.github.com" | grep -i "browser_download_url" | grep "aarch64.dmg" | cut -d '"' -f 4)
+        downloadURL=$(curl -sfL "$apiURL" | grep -i "browser_download_url" | grep "aarch64.dmg" | cut -d '"' -f 4)
     else
         # Intel (x86_64)
-        downloadURL=$(curl -sfL "https://api.github.com" | grep -i "browser_download_url" | grep "x86_64.dmg" | cut -d '"' -f 4)
+        downloadURL=$(curl -sfL "$apiURL" | grep -i "browser_download_url" | grep "x86_64.dmg" | cut -d '"' -f 4)
     fi
+    
     # Version ohne das "v" extrahieren
-    appNewVersion=$(curl -sfL "https://api.github.com" | grep "tag_name" | cut -d '"' -f 4 | tr -d 'v')
+    appNewVersion=$(curl -sfL "$apiURL" | grep "tag_name" | cut -d '"' -f 4 | tr -d 'v')
     archiveName="rustdesk-$appNewVersion.dmg"
     expectedTeamID="HZF9JMC8YN"
     ;;
+
 

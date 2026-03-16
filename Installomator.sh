b585430715d6ab4b1b6ef708ad4ba002e6fdaeba
hawkleon <hawkleon@users.noreply.github.com>
2026-03-16 09:20:34 +0000

    Runner compiled Installomator.sh and Labels.txt using assemble.sh

--- Installomator.sh
+++ Installomator.sh
@@ -7232,6 +7232,18 @@ microsoftlicensereset)
     downloadURL="https://office-reset.com"$(curl -fs https://office-reset.com/macadmins/ | grep -o -i "href.*\".*\"*License_Reset.*.pkg" | cut -d '"' -f2)
     expectedTeamID="QGS93ZLCU7"
     ;;
+microsoftoffice365_push)
+    name="MicrosoftOffice365"
+    type="pkg"
+    # Wir prüfen die Licensing-Komponente, um sicherzustellen, dass Office da ist
+    packageID="com.microsoft.pkg.licensing"
+    downloadURL="https://go.microsoft.com"
+    # Extrahiert die Version direkt aus dem Download-Link
+    appNewVersion=$(curl -fsIL "$downloadURL" | grep -i location: | grep -o "/Microsoft_.*pkg" | cut -d "_" -f 5)
+    expectedTeamID="UBF8T346G9"
+    # WICHTIG: Kein updateTool definiert -> Installomator lädt das volle PKG
+    blockingProcesses=( "Microsoft AutoUpdate" "Microsoft Word" "Microsoft PowerPoint" "Microsoft Excel" "Microsoft OneNote" "Microsoft Outlook" "OneDrive" "Teams" )
+    ;;
 microsoftoffice365)
     name="MicrosoftOffice365"
     type="pkg"

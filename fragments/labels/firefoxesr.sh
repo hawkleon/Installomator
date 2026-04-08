f915ad65baace8e98f90506f7321ef1f7f1855c1
hawkleon <leon.wiesen@hawk.de>
2026-04-08 09:50:16 +0200

    Update firefoxesr.sh
    anpassung 2, Label broken

--- fragments/labels/firefoxesr.sh
+++ fragments/labels/firefoxesr.sh
@@ -2,7 +2,7 @@ firefoxesr|\
 firefoxesrpkg)
     name="Firefox"
     type="pkg"
-    downloadURL="https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=osx&lang=de"
+    downloadURL="https://download.mozilla.org/?product=firefox-esr-pkg-latest-ssl&os=osx&lang=de"
     firefoxVersions=$(curl -fs "https://product-details.mozilla.org/1.0/firefox_versions.json")
     appNewVersion=$(getJSONValue "$firefoxVersions" "FIREFOX_ESR")
     expectedTeamID="43AQ936H96"

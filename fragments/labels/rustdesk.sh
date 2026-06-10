933f051595541c828e65ad4006f2ce75d29f0c9a
hawkleon <leon.wiesen@hawk.de>
2026-06-10 09:54:07 +0200

    Update rustdesk.sh
    Anpassung auf Version 1.4.7

--- fragments/labels/rustdesk.sh
+++ fragments/labels/rustdesk.sh
@@ -1,19 +1,17 @@
 rustdesk)
     name="RustDesk"
     type="dmg"
-    appNewVersion="1.4.6"
+    appNewVersion="1.4.7"
     
     if [[ "$(arch)" == "arm64" ]]; then
         # Apple Silicon (M1/M2/M3/M4)
-        downloadURL="https://github.com/rustdesk/rustdesk/releases/download/1.4.6/rustdesk-1.4.6-aarch64.dmg"
+        downloadURL="https://github.com/rustdesk/rustdesk/releases/download/1.4.7/rustdesk-1.4.7-aarch64.dmg"
     else
         # Intel Mac (x86_64)
-        downloadURL="https://github.com/rustdesk/rustdesk/releases/download/1.4.6/rustdesk-1.4.6-x86_64.dmg"
+        downloadURL="https://github.com/rustdesk/rustdesk/releases/download/1.4.7/rustdesk-1.4.7-x86_64.dmg"
     fi
     
     # archiveName muss eindeutig sein
-    archiveName="rustdesk-1.4.6-intel-fix.dmg"
+    archiveName="rustdesk-1.4.7-intel-fix.dmg"
     expectedTeamID="HZF9JMC8YN"
     ;;
-
-
